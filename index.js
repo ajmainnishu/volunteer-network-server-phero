a0d8132fcc7e3f89352ca72aaccf4a7de0c452cf
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-14 21:54:03 +0600

    feat: Volunteers get added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// mongodb server link
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -21,6 +22,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const volunteersCollection = client.db('usersDB').collection('volunteers');
+    // get volunteers data from mongodb
+    app.get('/volunteers', async (req, res) => {
+      const result = await volunteersCollection.find().toArray();
+      res.send(result);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
